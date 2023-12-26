cb98cdd99cc46319cdc5b8190be88f0d185c4c36
Euizzang8001 <a73875323@gmail.com>
2023-12-26 17:56:55 +0900

    myduoisok

--- back/routers/search.py
+++ back/routers/search.py
@@ -11,21 +11,21 @@ router = APIRouter(
 )
 
 
-@router.get('/get-player', response_model = str)
+@router.get('/get-player', response_model = str) #플레이어 이름 -> puuid get
 async def get_player_puuid(player:str, service: PlayerService=Depends()):
     result = service.get_player_puuid(player = player)
     return result
 
-@router.get('/get-matchid', response_model = list)
+@router.get('/get-matchid', response_model = list) #puuid -> matchid list return
 async def get_player_puuid(player_puuid:str, service: PlayerService=Depends()):
     result = service.get_match(player_puuid)
     return result
 
-#소환사 이름들이 같이 있는 match들의 id 정보 제공
-@router.post('/post-matchid', response_model=list)
-async def get_player_match_id(player: str, service: PlayerService = Depends()):
-    result = service.get_players_match(player)
-    return result
+# 소환사 이름들이 같이 있는 match들의 id 정보 제공
+# @router.post('/post-matchid', response_model=list)
+# async def get_player_match_id(player: str, service: PlayerService = Depends()):
+#     result = service.get_players_match(player)
+#     return result
 
 @router.get('/get-playerinfo-permatch', response_model=dict)
 async def get_player_match_id(players: list, match_id: str, service: PlayerService = Depends()):
--- front/main.py
+++ front/main.py
@@ -2,15 +2,8 @@ import streamlit as st
 import requests
 import datetime
 
-apikey="RGAPI-626d57b2-73d2-463a-9252-5d3b2f413799"
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": apikey
-}
+back_url = 'http://127.0.0.1:8000'
+get_puuid_url = back_url + 'get-player'
 
 st.title('My Duo Is OK..? :frowning:')
     
@@ -23,10 +16,16 @@ st.write(explain3)
 
 player = st.text_input('Write The Player Name')
 search_player = st.button('Search')
-player= player.replace('','%20')
-request_url = f'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{player}'
-result = requests.get(request_url, headers=headers)
-player_info = result.json()
+player_list = player.split(', ')
+
+match_id = []
+
+@router.get('/get-player', response_model = str) #플레이어 이름 -> puuid get
+async def get_player_puuid(player:str, service: PlayerService=Depends()):
+    result = service.get_player_puuid(player = player)
+    return result
+
 
 if search_player: #검색하기 위해 버튼을 누르면 검색 정보를 db에 저장하고 불러오기
-    st.write(player_info)
+    for i in range(len(player_list)):
+        match_list = requests(get_puuid_url, player = player_list[i])
